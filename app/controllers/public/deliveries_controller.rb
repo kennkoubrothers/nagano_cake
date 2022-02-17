17264cce4eb20ded64d25a25e929c92893447e9e
amiblog <reza.am.0425@gmail.com>
2022-02-17 03:56:09 +0000

    [add] 配送先一覧/登録/編集

--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -1,19 +1,35 @@
 class Public::DeliveriesController < ApplicationController
   def index
+    @delivery=Delivery.new
+    @deliveries=Delivery.all
   end
 
   def create
-
+    @delivery=Delivery.new(delivery_params)
+    @delivery.customer_id=current_user.id
+    @delivery.save
+    redirect request.referer
   end
 
   def edit
+    @delivery=Delivery.find(params[:id])
   end
 
   def update
-
+    @delivery=Delivery.find(params[:id])
+    @delivery.update(delivery_params)
+    redirect_to deliveries_path
   end
 
   def destroy
+    @delivery=Delivery.find(params[:id])
+    @delivery.destroy
+    redirect_to request.referer
+  end
 
+  private
+
+  def delivery_params
+    params.require(:delivery).perrmit(:postal_code, :address, :name)
   end
 end
