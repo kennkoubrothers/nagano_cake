79dc31186c63585dc93aacec2560503b3d4ceaf1
njguld95038043 <t.tamura6060@gmail.com>
2022-02-21 10:07:13 +0000

    Add 注文履歴詳細（管理者側）

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -12,8 +12,17 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    @order.update(order_params)
-    redirect_to admin_order_path(@order)
+    if @order.update(order_params)
+      if order_params[:status] == "confirm_deposit"
+        @order.order_details.update_all(making_status: 1)
+      end
+      redirect_to admin_order_path(@order)
+    else
+      @order_details = @order.order_details.all
+      @total = @order_details.inject(0) { |sum, item| sum + item.subtotal }
+      @postage = @order.postage
+      render :show
+    end
   end
 
   private
